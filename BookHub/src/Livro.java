980582ed860b671150724bd5a88f93f2dc06f1a9
Beatriz Abreu <beahabreu@gmail.com>
2025-03-24 19:23:50 -0300

    style: Adequação de variavel.

--- BookHub/src/Livro.java
+++ BookHub/src/Livro.java
@@ -1,19 +1,19 @@
 public class Livro {
     String titulo;
     String autor;
-    int anoDePublicacao;
+    int anoPublicacao;
 
     //Construtor
     public Livro(String titulo, String autor, int anoDePublicacao){
         this.titulo = titulo;
         this.autor = autor;
-        this.anoDePublicacao = anoDePublicacao;
+        this.anoPublicacao = anoDePublicacao;
     }
 
     public void exibirDetalhes(){
         System.out.println("Titulo: " + titulo + ".");
         System.out.println("Autor: " + autor + ".");
-        System.out.println("Ano de Publicação: " + anoDePublicacao + ".");
+        System.out.println("Ano de Publicação: " + anoPublicacao + ".");
         System.out.println("----------------------------");
         System.out.println(); //Cria uma linha em branco.
     }
