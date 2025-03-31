4f95fa32638bc452f343567d23552027aee081f3
Beatriz Abreu <beahabreu@gmail.com>
2025-03-31 20:31:15 -0300

    Correções

--- BookHub/src/app/Main.java
+++ BookHub/src/app/Main.java
@@ -5,9 +5,9 @@ import biblioteca.Livro;
 public class Main {
     public static void main(String[] args){
         Livro livro1 = new Livro("Pink Lemonade", "G.B. Baldassari", 2025 );
-        livro1.adicionarResenha("Um dos livros mais legais que já li! Trata com cuidado os sacrificios que algumas pessoas fazem por amor.", 4.8);
         Livro livro2 = new Livro ("De repente Namoradas", "G.B Baldassari", 2024);
         Livro livro3 = new Livro ("The Guardian", "Natália Moreira", 2020);
+        livro3.adicionarResenha("Eu amei ler esse livro antes mesmo dele ser livro kkkk, quando li a fic original já fiquei apaixonada.", 5);
 
         livro1.exibirDetalhes();
         livro2.exibirDetalhes();
