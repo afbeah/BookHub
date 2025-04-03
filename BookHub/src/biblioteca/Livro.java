66de2edc5f03759beffdd9fce83a6360f12f173a
Beatriz Abreu <beahabreu@gmail.com>
2025-04-03 16:17:31 -0300

    Feat: Inclusão de Getters e Setters

--- BookHub/src/biblioteca/Livro.java
+++ BookHub/src/biblioteca/Livro.java
@@ -19,11 +19,67 @@ public class Livro implements Detalhavel{
         this.editora = editora;
     }
 
+    //Getters
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+
+    public int getAnoPublicacao(){
+        return anoPublicacao;
+    }
+
+    public String getEditora(){
+        return editora;
+    }
+
+    public Resenha getResenha(){
+        return resenha;
+    }
+
+    //Setters
+    public void setTitulo(String titulo){
+        if(titulo != null && !titulo.isEmpty()){
+            this.titulo = titulo;
+        } else {
+            System.out.println("Título inválido!");
+        }
+    }
+
+    public void setAutor(String autor){
+        if(autor != null && !autor.isEmpty()){
+            this.autor = autor;
+        } else {
+            System.out.println("Autor inválido!");
+        }
+    }
+
+    public void setAnoPublicacao(int anoPublicacao){
+        if(anoPublicacao > 0){
+            this.anoPublicacao = anoPublicacao;
+        } else {
+            System.out.println("Ano de Publicação inválido!");
+            this.anoPublicacao = 0;
+        }
+    }
+
+    public void setEditora(String editora){
+        if(editora != null && editora.isEmpty()){
+            this.editora = editora;
+        } else {
+            System.out.println("Editora Inválida!");
+        }
+    }
+
+
     @Override
     public void exibirDetalhes(){
         System.out.println("Titulo: " + titulo + ".");
         System.out.println("Autor: " + autor + ".");
-        System.out.println("Ano de Publicação: " + anoPublicacao + ".");
+        System.out.println("Ano de Publicação: " +(anoPublicacao > 0 ? anoPublicacao : "Desconhecido") + ".");
         System.out.println("Editora: " + editora + ".");
 
         if(resenha != null){
